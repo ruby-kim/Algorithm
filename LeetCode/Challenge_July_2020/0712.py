05e909d6e73b9fcb7037141a7fc067487f5061ff
김경남 <devokkn@gmail.com>
2020-07-13 17:18:34 +0900

    Create 0712.py

---
+++ LeetCode/Challenge_July_2020/0712.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        """
+        1) Decimal to Binary num: bin()
+           n1 = 5
+           n1 = bin(5)      # '0b101'
+        2) [Random] to Decimal num: int()
+           n2 = 102         # 3 (tetra num)     
+           n2 = int(n2, 3)  # 11
+        3) '{0:0[num]b}'.format(N): print N, entire number of digits is [num]
+        """
+        return int('{0:032b}'.format(n)[::-1], 2)
