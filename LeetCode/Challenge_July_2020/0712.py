e9ed3c8659fe9479f782bc01514ef878c4172537
김경남 <devokkn@gmail.com>
2020-07-13 21:40:16 +0900

    Update 0712.py

--- LeetCode/Challenge_July_2020/0712.py
+++ LeetCode/Challenge_July_2020/0712.py
@@ -9,6 +9,6 @@ class Solution:
         2) [Random] to Decimal num: int()
            n2 = 102         # 3 (tetra num)     
            n2 = int(n2, 3)  # 11
-        3) '{0:0[num]b}'.format(N): print N, entire number of digits is [num]
+        3) '{0:[num]b}'.format(N): print N, entire number of digits is [num]
         """
         return int('{0:032b}'.format(n)[::-1], 2)
