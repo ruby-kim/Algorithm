7e4abe48acf8a7e4e747b2c08f5a1a9e0f419615
김경남 <devokkn@gmail.com>
2020-07-29 09:12:13 +0900

    Create 2445.cpp

---
+++ Baekjoon(BOJ)/2000~2999/2445.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N;
+    cin>>N;
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<=i; j++)
+            cout<<"*";
+        for(int j=0; j<2*(N-i)-2; j++)
+            cout<<" ";
+        for(int j=0; j<=i; j++)
+            cout<<"*";
+        cout<<endl;
+    }
+    for(int i=0; i<N-1; i++){
+        for(int j=0; j<N-1-i; j++)
+            cout<<"*";
+        for(int j=0; j<2*(i+1); j++)
+            cout<<" ";
+        for(int j=0; j<N-1-i; j++)
+            cout<<"*";
+        cout<<endl;
+    }
+
+    return 0;
+}
