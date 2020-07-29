a6239f34e3d63e57161af3da70a31b4e5151c10e
김경남 <devokkn@gmail.com>
2020-07-29 09:11:00 +0900

    Create 2444.cpp

---
+++ Baekjoon(BOJ)/2000~2999/2444.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int n,i,j,k;
+	scanf("%d", &n);
+
+	for(i=1; i<=n; i++)
+	{
+		for(j=1; j<=n-i; j++)
+			printf(" ");
+
+		for(k=1; k<=2*i-1; k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for(i=1; i<n; i++)
+	{
+		for(j=1; j<=i; j++)
+			printf(" ");
+		
+		for(k=1; k<=2*(n-i)-1; k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
