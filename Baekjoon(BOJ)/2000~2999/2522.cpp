de147b4a0be319409a3fc4124c0ca7b1097cb8db
김경남 <devokkn@gmail.com>
2020-07-29 09:13:30 +0900

    Create 2522.cpp

---
+++ Baekjoon(BOJ)/2000~2999/2522.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int n,i,j,k;
+
+	scanf("%d",&n);
+
+	for(i=1; i<=n; i++)
+	{
+		for(j=1; j<=n-i; j++)
+			printf(" ");
+
+		for(k=1; k<=i; k++)
+			printf("*");
+		printf("\n");
+	}
+
+	for(i=1; i<n; i++)
+	{
+		for(j=1; j<=i; j++)
+			printf(" ");
+		
+		for(k=1; k<=n-i; k++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
