61c28ca953dc21b914a39c56db4ca2f6a67b943d
김경남 <devokkn@gmail.com>
2020-08-17 21:23:31 +0900

    Create Jumping on the Clouds.py

---
+++ HackerRank/Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    if len(c) == 1: return 0
+    if len(c) == 2: return 0 if c[1] == 1 else 1
+    if c[2] == 1:
+        return 1 + jumpingOnClouds(c[1:])
+    if c[2] == 0:
+        return 1 + jumpingOnClouds(c[2:])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
