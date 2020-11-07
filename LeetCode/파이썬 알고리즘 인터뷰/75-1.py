8faf27f4288ac03356a8beaf0eeaaa76e55e7a9b
김경남 <devokkn@gmail.com>
2020-11-07 19:22:09 +0900

    Create 75-1.py

---
+++ LeetCode/파이썬 알고리즘 인터뷰/75-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """ 1) 브루트 포스로 계산: Time Limit Exceeded
+        Runtime: N/A
+        Memory: N/A
+        """
+        if not nums:
+            return nums
+        return [max(nums[i : i + k]) for i in range(len(nums) - k + 1)]
+        
+        
